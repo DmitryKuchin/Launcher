8e6de46949cecbddd0e0672a972d485c0b4ffed7
Kristofer Myollendorff <kuchin118@gmail.com>
2024-06-10 23:58:58 +0400

    Init data

--- config.ts
+++ config.ts
@@ -7,7 +7,7 @@ export const window = {
     resizable: false,
     maximizable: false,
     fullscreenable: false,
-    title: 'Aurora Launcher',
+    title: 'Leeroy\'s Machinarium',
 };
 
 export const api = {
